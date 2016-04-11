eb574b7c0394f3d0a5863d6e98c62326f39cdc5a
Orasio Spieler <spieler.orasio@gmail.com>
2016-04-11 07:04:22 +0300

    remove target

--- src/main/java/com/hp/mobile/Application.java
+++ src/main/java/com/hp/mobile/Application.java
@@ -1,10 +1,14 @@
 package com.hp.mobile;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 
 /**
  * Created by Spieler Orasio
@@ -15,16 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 //@Import(value = {MessageConfiguration.class,HttpSessionConfig.class} )
 //@EnableRabbit
 //@RabbitAutoConfiguration
-public class Application {
-    @Value("${server.port}")
-    private int serverPort;
-    @Value("${server.address}")
-    private String serverAddress;
+public class Application  {
+    @Autowired
+    Environment environment;
 
 
     @RequestMapping("/")
-    String hello() {
-        return "Hello Docker World : from ("+serverAddress + ":"+serverPort+")";
+    String hello() throws UnknownHostException {
+        return "Hello Docker World : from ("+ Inet4Address.getLocalHost().getHostAddress() + ":"+environment.getProperty("local.server.port")+")";
+
     }
 
     public static void main(String[] args) throws Exception {
--- target/classes/import.sql
+++
@@ -1,2 +0,0 @@
-insert into device(id, name) values (1, 'Nexus 5')
-insert into device(id, name) values (2,'Samsung 7')
